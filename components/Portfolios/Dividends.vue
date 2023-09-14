37aacb8326a9053c029e54d96910b7b429c78f67
serious_tom <cerniauskas.tomas68@gmail.com>
2023-09-14 03:17:30 +0300

    delete dividend

--- components/Portfolios/Dividends.vue
+++ components/Portfolios/Dividends.vue
@@ -15,15 +15,20 @@
         <tr v-if="dividendsLoading">
           <td colspan="5" class="text-center">Loading...</td>
         </tr>
-        <tr v-else v-for="dividend in dividends" :key="'dividends' + dividend.id">
+        <tr v-else v-for="(dividend, di) in dividends" :key="'dividends' + dividend.id">
           <td>{{ dividend.date }}</td>
           <td>{{ dividend.company.name }}</td>
           <td>{{ dividend.amount }} {{ dividend.currency.symbol }}</td>
           <td>{{ dividend.taxes_amount }} {{ dividend.currency.symbol }}</td>
           <td>{{ dividend.amount_after_taxes }} {{ dividend.currency.symbol }}</td>
           <td>
-            <a href=""><font-awesome-icon icon="fa-solid fa-pen" /></a>
-            <a href="" class="text-danger"><font-awesome-icon icon="fa-solid fa-trash-can" /></a>
+            <template v-if="!dividend.loading">
+              <a href=""><font-awesome-icon icon="fa-solid fa-pen" /></a>
+              <a href="" class="text-danger" @click.prevent="deleteDividend(di)"><font-awesome-icon icon="fa-solid fa-trash-can" /></a>
+            </template>
+            <template v-else>
+              Loading...
+            </template>
           </td>
         </tr>
       </tbody>
@@ -36,6 +41,8 @@
   </div>
 </template>
 <script setup>
+import useApiFetch from "../../composables/useApiFetch";
+
 const props = defineProps({
   portfolio: {
     type: Object,
@@ -53,11 +60,29 @@ const paginationData = ref({});
 const fetchDividends = (currentPage) => {
   dividendsLoading.value = true;
   useApiFetch('/dividends' + (props.portfolio ? '/' + props.portfolio.id : '') + (currentPage ? '?page=' + currentPage : '')).then((data) => {
-    dividends.value = data.data;
+    dividends.value = data.data.map((value) => {
+      value.loading = false;
+      return value;
+    });
     paginationData.value = data.meta;
     dividendsLoading.value = false;
   });
 };
+
+const deleteDividend = async (index) => {
+  window.confirm('Do you really wish to delete dividend?');
+
+  dividends.value[index].loading = true;
+
+  try {
+    await useApiFetch('/dividends/' + dividends.value[index].id + '/delete', {method: 'DELETE'});
+    dividends.value.splice(index, 1);
+    useNuxtApp().$toast.success('Dividend deleted');
+  } catch(e) {
+    useNuxtApp().$toast.error('Something wrong happened. Try again');
+  }
+  dividends.value[index].loading = false;
+}
 
 const pageChanged = async (page) => {
   const urlSearchParams = new URLSearchParams(window.location.search);
